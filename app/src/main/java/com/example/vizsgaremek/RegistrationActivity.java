8ac3c42599487d09867851346c10ab61fefc7b9e
Petrik Diák <diak@petrik.hu>
2023-02-17 13:00:47 +0100

    regisztracios oldal elkészítve és adatok bekérve róla

---
+++ app/src/main/java/com/example/vizsgaremek/RegistrationActivity.java
@@ -0,0 +1,36 @@
+package com.example.vizsgaremek;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class RegistrationActivity extends AppCompatActivity {
+
+    private EditText usernameTxt, emailTxt, passwordTxt, passwordagainTxt;
+    private Button registrationButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+
+        usernameTxt = findViewById(R.id.felhszanaloNev);
+        emailTxt = findViewById(R.id.email);
+        passwordTxt = findViewById(R.id.jelszo);
+        passwordagainTxt = findViewById(R.id.jelszoUjra);
+        registrationButton = findViewById(R.id.regisztralas);
+
+        registrationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = usernameTxt.getText().toString();
+                String email = emailTxt.getText().toString();
+                String password = passwordTxt.getText().toString();
+                String passwordAgain = passwordagainTxt.getText().toString();
+            }
+        });
+    }
+}
