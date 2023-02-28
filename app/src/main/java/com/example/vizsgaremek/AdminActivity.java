16c4edcd916bbab5780b1692d04171dde5ebe941
Petrik Diák <diak@petrik.hu>
2023-02-28 12:16:25 +0100

    alap activityk és funkciók kommentben

---
+++ app/src/main/java/com/example/vizsgaremek/AdminActivity.java
@@ -0,0 +1,17 @@
+package com.example.vizsgaremek;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AdminActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin);
+
+        //userek listázása és kezelése név alapján szűréssel
+        //user nézetre váltás lehetősége
+    }
+}
---
+++ app/src/main/java/com/example/vizsgaremek/UserMainActivity.java
@@ -0,0 +1,17 @@
+package com.example.vizsgaremek;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_main);
+
+        //Előre összerakott és a user által összerakott edzés tervek megjelenítése
+        //ha admin felhasználó az admin nézetre való váltás lehetősége
+    }
+}
