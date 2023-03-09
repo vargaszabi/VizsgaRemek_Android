78582388c360bf8731fd1798c171dd7e5037f470
Varga Szabolcs <vargaszabi2001@gmail.com>
2023-03-09 13:08:23 +0100

    User class, backend összekapcsolás félkész

--- app/src/main/java/com/example/vizsgaremek/RegistrationActivity.java
+++ app/src/main/java/com/example/vizsgaremek/RegistrationActivity.java
@@ -3,37 +3,74 @@ package com.example.vizsgaremek;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.gson.Gson;
+
+import java.io.IOException;
 
 public class RegistrationActivity extends AppCompatActivity {
 
     private EditText usernameTxt, emailTxt, passwordTxt, passwordagainTxt;
     private Button registrationButton, backButton;
+    private String BASE_URL="http://10.0.2.2:3000/auth/register";
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_registration);
-
+    public void init(){
         usernameTxt = findViewById(R.id.felhszanaloNev);
         emailTxt = findViewById(R.id.email);
         passwordTxt = findViewById(R.id.jelszo);
         passwordagainTxt = findViewById(R.id.jelszoUjra);
         registrationButton = findViewById(R.id.regisztralas);
         backButton = findViewById(R.id.back);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+        init();
 
         registrationButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String username = usernameTxt.getText().toString();
-                String email = emailTxt.getText().toString();
-                String password = passwordTxt.getText().toString();
-                String passwordAgain = passwordagainTxt.getText().toString();
-                Intent loginpage = new Intent(RegistrationActivity.this, LoginActivity.class);
-                startActivity(loginpage);
+                String username = usernameTxt.getText().toString().trim();
+                String email = emailTxt.getText().toString().trim();
+                String password = passwordTxt.getText().toString().trim();
+                String passwordAgain = passwordagainTxt.getText().toString().trim();
+                if (username.isEmpty()){
+                    usernameTxt.setError("Nem lehet üres");
+                } else {
+                    usernameTxt.setError(null);
+                }
+                if (email.isEmpty()){
+                    emailTxt.setError("Nem lehet üres");
+                } else {
+                    emailTxt.setError(null);
+                }
+                if (password.isEmpty()){
+                    passwordTxt.setError("Nem lehet üres");
+                } else {
+                    passwordTxt.setError(null);
+                }
+                if (passwordAgain.isEmpty()){
+                    passwordagainTxt.setError("Nem lehet üres");
+                } else {
+                    passwordagainTxt.setError(null);
+                }
+                if (password.equals(passwordAgain)){
+                    passwordagainTxt.setError("A jelszó nem egyezik!");
+                } else {
+                    passwordagainTxt.setError(null);
+                }
+                Gson json = new Gson();
+                Users newUser = new Users(username, email, password, passwordAgain);
+                RequestTask task = new RequestTask(BASE_URL, "POST", json.toJson(newUser));
+                task.execute;
             }
         });
         backButton.setOnClickListener(new View.OnClickListener() {
@@ -43,5 +80,80 @@ public class RegistrationActivity extends AppCompatActivity {
                 startActivity(loginpage);
             }
         });
+    }
+
+    private class RequestTask extends AsyncTask<Void, Void, Response> {
+        String requesrtUrl;
+        String requestType;
+        String requestParams;
+
+        public RequestTask(String requesrtUrl, String requestType, String requestParams) {
+            this.requesrtUrl = requesrtUrl;
+            this.requestType = requestType;
+            this.requestParams = requestParams;
+        }
+
+        public RequestTask(String requesrtUrl, String requestType) {
+            this.requesrtUrl = requesrtUrl;
+            this.requestType = requestType;
+        }
+
+        @Override
+        protected Response doInBackground(Void... voids) {
+            Response response = null;
+            try {
+                switch (requestType) {
+                    case "GET":
+                        response = RequestHandler.get(requesrtUrl);
+                        break;
+                    case "POST":
+                        response = RequestHandler.post(requesrtUrl, requestParams);
+                        break;
+                    case "PUT":
+                        response = RequestHandler.put(requesrtUrl,requestParams);
+                        break;
+                    case "DELETE":
+                        response = RequestHandler.delete(requesrtUrl + "/" + requestParams);
+                        break;
+                }
+            } catch (IOException e){
+                Toast.makeText(RegistrationActivity.this,
+                        e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+            return response;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPostExecute();
+        }
+
+        @Override
+        protected void onPostExecute(Response response){
+            super.onPostExecute(response);
+            if (response.getResponseCode() >= 400) {
+                Toast.makeText(RegistrationActivity.this,
+                        response.getContent(), Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(RegistrationActivity.this,
+                       "Sikeres Regisztráció", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(RegistrationActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+            switch (requestType) {
+                case "GET":
+
+                    break;
+                case "POST":
+
+                    break;
+                case "PUT":
+
+                    break;
+                case "DELETE":
+
+                    break;
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/vizsgaremek/Users.java
@@ -0,0 +1,47 @@
+package com.example.vizsgaremek;
+
+public class Users {
+    String username;
+    String email;
+    String password;
+    String passwordAgain;
+
+    public Users(String username, String email, String password, String passwordAgain) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.passwordAgain = passwordAgain;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPasswordAgain() {
+        return passwordAgain;
+    }
+
+    public void setPasswordAgain(String passwordAgain) {
+        this.passwordAgain = passwordAgain;
+    }
+}
