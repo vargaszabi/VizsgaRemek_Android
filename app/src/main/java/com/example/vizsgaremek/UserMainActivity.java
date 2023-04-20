7f57d0b1972e092e754f83ef060491146da80517
petrikdiak <diak@petrik.hu>
2023-04-20 15:33:27 +0200

    gombok inicializása

--- app/src/main/java/com/example/vizsgaremek/UserMainActivity.java
+++ app/src/main/java/com/example/vizsgaremek/UserMainActivity.java
@@ -2,16 +2,49 @@ package com.example.vizsgaremek;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class UserMainActivity extends AppCompatActivity {
+
+    private Button kezdoButton, gyakorlottButton, rutinosButton;
+
+    public void init() {
+        kezdoButton = findViewById(R.id.kezdo);
+        gyakorlottButton = findViewById(R.id.gyakorlott);
+        rutinosButton = findViewById(R.id.rutinos);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_main);
+        init();
 
-        //Előre összerakott és a user által összerakott edzés tervek megjelenítése
-        //ha admin felhasználó az admin nézetre való váltás lehetősége
+        kezdoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent exercisepgae = new Intent(UserMainActivity.this, UserExerciseActivity.class);
+                startActivity(exercisepgae);
+            }
+        });
+
+        gyakorlottButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent exercisepgae = new Intent(UserMainActivity.this, UserExerciseActivity.class);
+                startActivity(exercisepgae);
+            }
+        });
+
+        rutinosButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent exercisepgae = new Intent(UserMainActivity.this, UserExerciseActivity.class);
+                startActivity(exercisepgae);
+            }
+        });
     }
 }
