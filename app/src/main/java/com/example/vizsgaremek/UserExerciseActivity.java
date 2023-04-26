a7766e0b080869b5fe7885250ade363c8e092f3e
Varga Szabolcs <vargaszabi2001@gmail.com>
2023-04-26 09:17:47 +0200

    gyakorlat listázás elkezdve

--- app/src/main/java/com/example/vizsgaremek/UserExerciseActivity.java
+++ app/src/main/java/com/example/vizsgaremek/UserExerciseActivity.java
@@ -1,16 +1,41 @@
 package com.example.vizsgaremek;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.util.ArrayList;
+import java.util.List;
 
 public class UserExerciseActivity extends AppCompatActivity {
+
+    private String viedo_URL= "";
+    private String BASE_URL = "";
+    private View desc;
+    private Button nextButton;
+
+    public void init(){
+        nextButton = findViewById(R.id.nextgyak);
+        desc = findViewById(R.id.leiras);
+        List excerciseList= new ArrayList();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_exercise);
+        init();
 
-        //edzésterv követése
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String desc;
+                Intent reloadpage = new Intent(UserExerciseActivity.this, UserExerciseActivity.class);
+                startActivity(reloadpage);
+            }
+        });
     }
 }
